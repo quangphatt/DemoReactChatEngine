23fcf05a0fc26977fa3219b9bc393c9f3763a723
Quang Phat <phatquang2811@gmail.com>
2022-03-01 07:34:16 +0700

    add chat component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from 'react-chat-engine'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+      publicKey={'dc8ae624-ea3e-4ec7-92f4-90e7dfb2db22'}
+      userName={'nam_tran'}
+      userSecret={'namdeptrai123'}
+    />
   );
 }
 
